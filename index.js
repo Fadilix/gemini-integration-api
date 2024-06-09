46319405d4fda03cb807d6f167c9a06604a42fb4
Fadilix <iamfadile2@gmail.com>
2024-06-09 08:38:08 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+app.get("/", (req, res) => {
+    return res.status(200).json({ msg: "This is a gemini integration api" });
+})
+
+const callGemini = async (prompt) => {
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+    return text;
+}
+
+
+app.post("/gemini", async (req, res) => {
+    const { prompt } = req.body;
+    const result = await callGemini(prompt);
+    try {
+        return res.status(200).json({ result });
+    } catch (err) {
+        return res.status(500).json({ message: "Internal Server Error" })
+    }
+});
+
+
+const { PORT } = process.env
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
